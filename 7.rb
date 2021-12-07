3a5a97a8e808eb2aaacda02f0bf638921c6059df
Kurtisz Krisztián <kurtisz.krisztian@electool.com>
2021-12-07 21:39:04 +0100

    day 7

---
+++ 7.rb
@@ -0,0 +1,21 @@
+base_name = '7'
+pos = File.read("./#{base_name}.txt", chomp: true).split(',').map(&:to_i)
+
+def smallest_dist(pos)
+  dist = 0
+  (pos.min..pos.max).each do |pos_val|
+    fuel = pos.inject(0) do |sum, n|
+      # sum + (n - pos_val).abs # part1
+      sum + (1..(n - pos_val).abs).sum
+    end  
+    if dist > 0
+      dist = [fuel, dist].min
+    else
+      dist = 1 * fuel
+    end
+  end
+  dist
+end
+
+puts smallest_dist(pos)
+
